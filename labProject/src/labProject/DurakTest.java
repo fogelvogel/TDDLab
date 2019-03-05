ff16daf4b4be95c4bca3831eb33a0b94d1a67ac3
Elena Popova <44632346+fogelvogel@users.noreply.github.com>
2019-03-05 14:50:16 +0700

    Добавлен тест создания шести карт

--- labProject/src/labProject/DurakTest.java
+++ labProject/src/labProject/DurakTest.java
@@ -65,4 +65,10 @@ public class DurakTest {
 		}
 		assertFalse(wrongNumber);
 	}
+	
+	@Test
+	public void testOfCreationSixCardsByDurak() {
+		Card[] array = Durak.createSixCard();
+		assertNotNull(array);
+	}
 }
