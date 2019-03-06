711ca6372c482359c6c75fd8254e51e0ca0cb97d
Elena Popova <44632346+fogelvogel@users.noreply.github.com>
2019-03-06 09:45:25 +0700

    Добавлен тест наличия функции "Играть"

--- labProject/src/labProject/PlayerTest.java
+++ labProject/src/labProject/PlayerTest.java
@@ -1,5 +1,21 @@
 package labProject;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class PlayerTest {
 
+	@Test
+	public void testOfCreationPlayer() {
+		Player instance = new Player();
+		assertNotNull(instance);
+	}
+	
+	@Test
+	public void testOfPlaying() {
+		Player instance = new Player();
+		int success = instance.play();
+		assertEquals(success, 1);
+	}
 }
