b7d5777fb1c936eb6382c3f497628ee1a4263069
Elena Popova <44632346+fogelvogel@users.noreply.github.com>
2019-03-07 18:52:07 +0700

    Улучшена функция выкладывания карты на стол

--- labProject/src/labProject/Player.java
+++ labProject/src/labProject/Player.java
@@ -14,13 +14,18 @@ public class Player {
 		this.cards = cards;
 	}
 	public void giveCard() {
-		Card[] tmp = new Card[] {this.cards[0], this.cards[1], this.cards[2], this.cards[3], this.cards[4], this.cards[5]};
-		this.cards = new Card[5];
+		Card[] tmp = new Card[this.cards.length-1];
 		
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < this.cards.length - 1; i++) {
+			tmp[i] = this.cards[i];
+		}
+		this.cards = new Card[tmp.length];
+		
+		for (int i = 0; i < tmp.length; i++) {
 			this.cards[i] = tmp[i];
 		}
 	}
+	
 	public Card[] getCards() {
 	
 		return this.cards;
